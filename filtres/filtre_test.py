997e3bf85ba340929a91171f19c8ddfe5955b910
Bruno <bguerin@dawan.fr>
2024-03-22 14:41:29 +0100

    [UPDATE] Mieux !

--- filtres/filtre_test.py
+++ filtres/filtre_test.py
@@ -2,13 +2,13 @@
 # -*- encoding: utf8 -*-
 #
 
-def msg(data,str):
-    return data + ' ' + str
-
 class FilterModule(object):
     ''' Give back filters to Ansible '''
 
     def filters(self):
         return {
-            'filtre_test': msg
+            'filtre_test': self.msg
         }
+
+    def msg(self,data,str):
+        return data + ' ' + str
